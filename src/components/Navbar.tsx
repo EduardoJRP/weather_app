993003ace2ed564a1bc7f1ad6bd1741063696204
Eduardo Rojas <edu.rojas.paez@gmail.com>
2024-04-15 15:30:16 -0500

    update: search bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaLocationArrow, FaSun } from 'react-icons/fa';
 import { FaLocationDot } from 'react-icons/fa6';
 import SearchBox from './SearchBox';
 
 export default function Navbar() {
+  const [city, setCity] = useState('');
+  const [error, setError] = useState('');
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  async function handleInputChange(value: string) {
+    setCity(value);
+  }
+
   return (
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
       <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
@@ -17,7 +26,11 @@ export default function Navbar() {
           <FaLocationArrow className="text-2xl" />
           <p className="text-slate-900/80 text-sm">Colombia</p>
           <div>
-            <SearchBox value={''} onChange={undefined} onSubmit={undefined} />
+            <SearchBox
+              value={city}
+              onChange={handleInputChange(e.target.value)}
+              onSubmit={undefined}
+            />
           </div>
         </section>
       </div>
